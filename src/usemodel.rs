7fdb1883a7439dcab29508221d6295237febfb31
KevinAvisi <k.schomper@avisi.nl>
2022-05-31 15:22:42 +0200

    Final version

--- src/usemodel.rs
+++ src/usemodel.rs
@@ -1,32 +1,106 @@
 use std::fs::File;
 use std::io::{Read, Write};
 
-use ndarray::{Array, Axis};
-use smartcore::decomposition::pca::{PCA, PCAParameters};
+use ndarray::{arr1, Array, Array1, Axis};
+use num_traits::ToPrimitive;
+use serde::Serialize;
+use smartcore::cluster::dbscan::{DBSCAN, DBSCANParameters};
+use smartcore::cluster::kmeans::{KMeans, KMeansParameters};
+use smartcore::ensemble::random_forest_classifier::{
+    RandomForestClassifier, RandomForestClassifierParameters,
+};
 use smartcore::linalg::naive::dense_matrix::DenseMatrix;
 use smartcore::linear::linear_regression::LinearRegression;
+use smartcore::linear::logistic_regression::LogisticRegression;
 use smartcore::math::distance::euclidian;
-use smartcore::metrics::{accuracy, mean_squared_error, roc_auc_score};
+use smartcore::math::distance::euclidian::Euclidian;
+use smartcore::metrics::{accuracy, completeness_score, f1, homogeneity_score, mean_squared_error, recall, roc_auc_score, v_measure_score};
 use smartcore::model_selection::train_test_split;
 use smartcore::neighbors::knn_classifier::KNNClassifier;
+use smartcore::tree::decision_tree_classifier::{
+    DecisionTreeClassifier, DecisionTreeClassifierParameters,
+};
 
-use crate::smartcoreknn::{fill_dataset_return_y, get_dataset, get_dataset_kevin, get_tracks, make_meshgrid, scatterplot, scatterplot_vec, scatterplot_with_mesh};
+use crate::utilities::{get_dataset, get_dataset_kevin, performance_graph, scatterplot, to_array2};
 
-mod smartcoreknn;
+mod utilities;
 
-fn main() {
-    let model = train_lr();
-    {
-        let bytes = bincode::serialize(&model).expect("Can not serialize the model");
-        File::create("lr2.model")
-            .and_then(|mut f| f.write_all(&bytes))
-            .expect("Can not persist model");
+struct Track {
+    danceability: f64,
+    energy: f64,
+    key: i64,
+    loudness: f64,
+    mode: i64,
+    speechiness: f64,
+    instrumentalness: f64,
+    liveness: f64,
+    valence: f64,
+    tempo: f64,
+    time_signature: i64,
+}
+
+trait ToArray {
+    fn to_array(&self) -> Array1<f64>;
+}
+
+impl ToArray for Track {
+    fn to_array(&self) -> Array1<f64> {
+        let vector = Vec::from([
+            self.danceability,
+            self.energy,
+            self.key.to_f64().unwrap(),
+            self.loudness,
+            self.mode.to_f64().unwrap(),
+            self.speechiness,
+            self.instrumentalness,
+            self.liveness,
+            self.valence,
+            self.tempo,
+            self.time_signature.to_f64().unwrap(),
+        ]);
+        return Array::from(arr1(&*vector));
     }
 }
 
+fn main() {
+    let dt: DecisionTreeClassifier<f64> = {
+        let mut buf: Vec<u8> = Vec::new();
+        File::open("dt.model")
+            .and_then(|mut f| f.read_to_end(&mut buf))
+            .expect("Can not load model");
+        bincode::deserialize(&buf).expect("Can not deserialize the model")
+    };
+
+    let track = Track {
+        danceability: 0.382,
+        energy: 0.354,
+        key: 7,
+        loudness: -6.595,
+        mode: 1,
+        speechiness: 0.0289,
+        instrumentalness: 3.11e-06,
+        liveness: 0.168,
+        valence: 0.435,
+        tempo: 87.018,
+        time_signature: 4
+    };
+    let array = track.to_array();
+    let vec = vec!(array);
+    let array2d = to_array2(vec.as_slice()).unwrap();
+    let x = DenseMatrix::from_array(
+        1,
+        array2d.len_of(Axis(1)),
+        array2d.as_slice().unwrap(),
+    );
+    let prediction = dt.predict(&x).unwrap();
+    println!("{:?}", prediction);
+}
+
+#[allow(dead_code)]
 fn train_knn() {
     let (dataset, y) = get_dataset_kevin();
-    let records = Array::from_shape_vec(dataset.raw_dim(), dataset.iter().cloned().collect()).unwrap();
+    let records =
+        Array::from_shape_vec(dataset.raw_dim(), dataset.iter().cloned().collect()).unwrap();
     let x = DenseMatrix::from_array(
         y.len(),
         records.len_of(Axis(1)),
@@ -35,20 +109,145 @@ fn train_knn() {
     let (x_train, x_test, y_train, y_test) = train_test_split(&x, &y, 0.2, true);
     let knn = KNNClassifier::fit(&x_train, &y_train, Default::default()).unwrap();
     let predictions = knn.predict(&x_test).unwrap();
+    let predict_train = knn.predict(&x_train).unwrap();
     println!("MSE: {}", mean_squared_error(&y_test, &predictions));
     println!("accuracy: {}", accuracy(&y_test, &predictions));
 
-    scatterplot(
-        &x_train,
-        Some(&predictions.into_iter().map(|f| f as usize).collect()),
-        "test",
-    )
-        .unwrap();
+    // scatterplot(
+    //     &x_train,
+    //     Some(&predictions.into_iter().map(|f| f as usize).collect()),
+    //     "test",
+    // )
+    //     .unwrap();
+
+    let test_acc = accuracy(&y_test, &predictions);
+
+    let train_acc = accuracy(&y_train, &predict_train);
+    let accuracy = vec![(test_acc * 100.0f64) as i32, (train_acc * 100.0f64) as i32];
+    println!("{:?}", accuracy);
+    performance_graph("knn_accuracy.svg", "accuracy", accuracy);
+
+    let test_f1 = f1(&y_test, &predictions, 1.0);
+
+    let train_f1 = f1(&y_train, &predict_train, 1.0);
+    let f1 = vec![(test_f1 * 100.0f64) as i32, (train_f1 * 100.0f64) as i32];
+    println!("{:?}", f1);
+    performance_graph("knn_f1.svg", "F1 Score", f1);
+
+    let data = recall(&y_test, &predictions);
+
+    let compare = recall(&y_train, &predict_train);
+    let f1 = vec![(data * 100.0f64) as i32, (compare * 100.0f64) as i32];
+    println!("{:?}", f1);
+    performance_graph("kn_recall.svg", "Recall Score", f1)
+        .expect("Write of recall graph was not successful");
 }
 
-fn train_lr() -> LinearRegression<f64, DenseMatrix<f64>> {
+fn train_dt() -> DecisionTreeClassifier<f64> {
     let (dataset, y) = get_dataset_kevin();
-    let records = Array::from_shape_vec(dataset.raw_dim(), dataset.iter().cloned().collect()).unwrap();
+    let records =
+        Array::from_shape_vec(dataset.raw_dim(), dataset.iter().cloned().collect()).unwrap();
+    let x = DenseMatrix::from_array(
+        y.len(),
+        records.len_of(Axis(1)),
+        dataset.as_slice().unwrap(),
+    );
+    let (x_train, x_test, y_train, y_test) = train_test_split(&x, &y, 0.2, true);
+    let dt = DecisionTreeClassifier::fit(
+        &x_train,
+        &y_train,
+        DecisionTreeClassifierParameters::default(),
+    )
+        .unwrap();
+    let predictions = dt.predict(&x_test).unwrap();
+    let predict_train = dt.predict(&x_train).unwrap();
+
+    let test_acc = accuracy(&y_test, &predictions);
+
+    let train_acc = accuracy(&y_train, &predict_train);
+    let accuracy = vec![(test_acc * 100.0f64) as i32, (train_acc * 100.0f64) as i32];
+    println!("{:?}", accuracy);
+    performance_graph("dt_accuracy.svg", "accuracy", accuracy)
+        .expect("Write of accuracy graph was not successful");
+
+    let test_f1 = f1(&y_test, &predictions, 1.0);
+
+    let train_f1 = f1(&y_train, &predict_train, 1.0);
+    let f1 = vec![(test_f1 * 100.0f64) as i32, (train_f1 * 100.0f64) as i32];
+    println!("{:?}", f1);
+    performance_graph("dt_f1.svg", "F1 Score", f1)
+        .expect("Write of f1 graph was not successful");
+
+    let data = recall(&y_test, &predictions);
+
+    let compare = recall(&y_train, &predict_train);
+    let f1 = vec![(data * 100.0f64) as i32, (compare * 100.0f64) as i32];
+    println!("{:?}", f1);
+    performance_graph("dt_recall.svg", "Recall Score", f1)
+        .expect("Write of recall graph was not successful");
+    dt
+}
+
+#[allow(dead_code)]
+fn train_rf() -> RandomForestClassifier<f64> {
+    let (dataset, y) = get_dataset_kevin();
+    let records =
+        Array::from_shape_vec(dataset.raw_dim(), dataset.iter().cloned().collect()).unwrap();
+    let x = DenseMatrix::from_array(
+        y.len(),
+        records.len_of(Axis(1)),
+        dataset.as_slice().unwrap(),
+    );
+    let (x_train, x_test, y_train, y_test) = train_test_split(&x, &y, 0.2, true);
+    let rf = RandomForestClassifier::fit(
+        &x_train,
+        &y_train,
+        RandomForestClassifierParameters::default(),
+    )
+        .unwrap();
+    let predictions = rf.predict(&x_test).unwrap();
+    let predict_train = rf.predict(&x_train).unwrap();
+    println!("MSE: {}", mean_squared_error(&y_test, &predictions));
+    println!("accuracy: {}", accuracy(&y_test, &predictions));
+
+    // scatterplot(
+    //     &x_train,
+    //     Some(&predictions.into_iter().map(|f| f as usize).collect()),
+    //     "test",
+    // )
+    //     .unwrap();
+
+    let test_acc = accuracy(&y_test, &predictions);
+
+    let train_acc = accuracy(&y_train, &predict_train);
+    let accuracy = vec![(test_acc * 100.0f64) as i32, (train_acc * 100.0f64) as i32];
+    println!("{:?}", accuracy);
+    performance_graph("rf_accuracy.svg", "accuracy", accuracy)
+        .expect("Write of accuracy graph was not successful");
+
+    let data = f1(&y_test, &predictions, 1.0);
+
+    let compare = f1(&y_train, &predict_train, 1.0);
+    let f1 = vec![(data * 100.0f64) as i32, (compare * 100.0f64) as i32];
+    println!("{:?}", f1);
+    performance_graph("rf_f1.svg", "F1 Score", f1)
+        .expect("Write of f1 graph was not successful");
+
+    let data = recall(&y_test, &predictions);
+
+    let compare = recall(&y_train, &predict_train);
+    let f1 = vec![(data * 100.0f64) as i32, (compare * 100.0f64) as i32];
+    println!("{:?}", f1);
+    performance_graph("rf_recall.svg", "Recall Score", f1)
+        .expect("Write of recall graph was not successful");
+    rf
+}
+
+#[allow(dead_code)]
+fn train_lir() -> LinearRegression<f64, DenseMatrix<f64>> {
+    let (dataset, y) = get_dataset_kevin();
+    let records =
+        Array::from_shape_vec(dataset.raw_dim(), dataset.iter().cloned().collect()).unwrap();
     let x = DenseMatrix::from_array(
         y.len(),
         records.len_of(Axis(1)),
@@ -56,6 +255,65 @@ fn train_lr() -> LinearRegression<f64, DenseMatrix<f64>> {
     );
     let (x_train, x_test, y_train, y_test) = train_test_split(&x, &y, 0.2, true);
     let lr = LinearRegression::fit(&x_train, &y_train, Default::default()).unwrap();
+    let predictions = lr.predict(&x_test).unwrap();
+    let predictions_training = lr.predict(&x_train).unwrap();
+
+    let mut predict_test: Vec<f64> = Vec::new();
+    let mut i = 0;
+    for float in predictions.as_slice() {
+        predict_test.insert(i as usize, f64::round(*float));
+        i += 1;
+    }
+
+    let mut predict_train: Vec<f64> = Vec::new();
+    let mut i = 0;
+    for float in predictions_training.as_slice() {
+        predict_train.insert(i as usize, f64::round(*float));
+        i += 1;
+    }
+
+    println!("{:?}", predict_test);
+    println!("roc auc: {}", roc_auc_score(&y_test, &predictions));
+    println!("accuracy: {}", accuracy(&y_test, &predict_test));
+    let data = accuracy(&y_test, &predict_test);
+
+    let compare = accuracy(&y_train, &predict_train);
+    let accuracy = vec![(data * 100.0f64) as i32, (compare * 100.0f64) as i32];
+    println!("{:?}", accuracy);
+    performance_graph("lin_accuracy.svg", "accuracy", accuracy)
+        .expect("Write of accuracy graph was not successful");
+
+    let data = f1(&y_test, &predict_test, 1.0);
+
+    let compare = f1(&y_train, &predict_train, 1.0);
+    let f1 = vec![(data * 100.0f64) as i32, (compare * 100.0f64) as i32];
+    println!("{:?}", f1);
+    performance_graph("lin_f1.svg", "F1 Score", f1)
+        .expect("Write of f1 graph was not successful");
+
+    let data = recall(&y_test, &predict_test);
+
+    let compare = recall(&y_train, &predict_train);
+    let recall = vec![(data * 100.0f64) as i32, (compare * 100.0f64) as i32];
+    println!("{:?}", recall);
+    performance_graph("lin_recall.svg", "Recall Score", recall)
+        .expect("Write of recall graph was not successful");
+
+    lr
+}
+
+#[allow(dead_code)]
+fn train_lor() -> LogisticRegression<f64, DenseMatrix<f64>> {
+    let (dataset, y) = get_dataset_kevin();
+    let records =
+        Array::from_shape_vec(dataset.raw_dim(), dataset.iter().cloned().collect()).unwrap();
+    let x = DenseMatrix::from_array(
+        y.len(),
+        records.len_of(Axis(1)),
+        dataset.as_slice().unwrap(),
+    );
+    let (x_train, x_test, y_train, y_test) = train_test_split(&x, &y, 0.2, true);
+    let lr = LogisticRegression::fit(&x_train, &y_train, Default::default()).unwrap();
     let predictions = lr.predict(&x_test).unwrap();
 
     let mut predicvector: Vec<f64> = Vec::new();
@@ -69,30 +327,105 @@ fn train_lr() -> LinearRegression<f64, DenseMatrix<f64>> {
     println!("roc auc: {}", roc_auc_score(&y_test, &predictions));
     println!("accuracy: {}", accuracy(&y_test, &predicvector));
 
-    scatterplot_vec(
-        predicvector,
-        &y_test,
-        "lr",
-    );
+    // scatterplot_vec(
+    //     predicvector,
+    //     &y_test,
+    //     "lor",
+    // );
 
+    let data = accuracy(&y_test, &predictions);
+
+    let compare = accuracy(&y_train, &lr.predict(&x_train).unwrap());
+    let accuracy = vec![(data * 100.0f64) as i32, (compare * 100.0f64) as i32];
+    println!("{:?}", accuracy);
+    performance_graph("log_accuracy.svg", "accuracy", accuracy)
+        .expect("Write of accuracy graph was not successful");
+
+    let data = f1(&y_test, &predictions, 1.0);
+
+    let compare = f1(&y_train, &lr.predict(&x_train).unwrap(), 1.0);
+    let f1 = vec![(data * 100.0f64) as i32, (compare * 100.0f64) as i32];
+    performance_graph("log_f1.svg", "F1 Score", f1)
+        .expect("Write of f1 graph was not successful");
+
+    let data = recall(&y_test, &predictions);
+    let compare = recall(&y_train, &lr.predict(&x_train).unwrap());
+    let recall = vec![(data * 100.0f64) as i32, (compare * 100.0f64) as i32];
+    println!("{:?}", recall);
+    performance_graph("log_recall.svg", "Recall Score", recall)
+        .expect("Write of recall graph was not successful");
     lr
 }
 
-fn print_accuracy() {
-    let knn: KNNClassifier<f64, euclidian::Euclidian> = {
-        let mut buf: Vec<u8> = Vec::new();
-        File::open("knn.model")
-            .and_then(|mut f| f.read_to_end(&mut buf))
-            .expect("Can not load model");
-        bincode::deserialize(&buf).expect("Can not deserialize the model")
-    };
-
-    let (dataset, y) = get_dataset("marcel");
+#[allow(dead_code)]
+fn kmeans() -> KMeans<f64> {
+    let (dataset, y) = get_dataset_kevin();
+    let records =
+        Array::from_shape_vec(dataset.raw_dim(), dataset.iter().cloned().collect()).unwrap();
     let x = DenseMatrix::from_array(
         y.len(),
-        dataset.len_of(Axis(1)),
+        records.len_of(Axis(1)),
         dataset.as_slice().unwrap(),
     );
-    let prediction = knn.predict(&x).unwrap();
-    println!("accuracy: {}", accuracy(&y, &prediction));
+    let (_x_train, _x_test, _y_train, _y_test) = train_test_split(&x, &y, 0.2, true);
+    // These are our target class labels
+    // Fit & predict
+    let kmeans = KMeans::fit(&x, KMeansParameters::default().with_k(7)).unwrap();
+    let predictions = kmeans.predict(&x).unwrap();
+
+    // Measure performance
+    println!("Homogeneity: {}", homogeneity_score(&y, &predictions));
+    println!("Completeness: {}", completeness_score(&y, &predictions));
+    println!("V Measure: {}", v_measure_score(&y, &predictions));
+
+    scatterplot(
+        &x,
+        Some(&predictions.into_iter().map(|f| f as usize).collect()),
+        "kmeansclusters",
+    ).expect("couldn't write graph kmeans");
+
+    kmeans
+}
+
+#[allow(dead_code)]
+fn dbscan() -> DBSCAN<f64, Euclidian> {
+    let (dataset, y) = get_dataset_kevin();
+    let records =
+        Array::from_shape_vec(dataset.raw_dim(), dataset.iter().cloned().collect()).unwrap();
+    let x = DenseMatrix::from_array(
+        y.len(),
+        records.len_of(Axis(1)),
+        dataset.as_slice().unwrap(),
+    );
+    let (_x_train, _x_test, _y_train, _y_test) = train_test_split(&x, &y, 0.2, true);
+    // These are our target class labels
+    // Fit & predict
+    let dbscan = DBSCAN::fit(
+        &x,
+        DBSCANParameters::default()
+            .with_eps(0.2)
+            .with_min_samples(7),
+    )
+        .unwrap();
+    let predictions = dbscan.predict(&x).unwrap();
+
+    // Measure performance
+    println!("Homogeneity: {}", homogeneity_score(&y, &predictions));
+    println!("Completeness: {}", completeness_score(&y, &predictions));
+    println!("V Measure: {}", v_measure_score(&y, &predictions));
+
+    scatterplot(
+        &x,
+        Some(&predictions.into_iter().map(|f| f as usize).collect()),
+        "dbscan",
+    ).expect("couldn't write graph dbscan");
+
+    dbscan
+}
+
+fn write_model<T: Serialize>(model: T, path: &str) {
+    let bytes = bincode::serialize(&model).expect("Can not serialize the model");
+    File::create(path)
+        .and_then(|mut f| f.write_all(&bytes))
+        .expect("Can not persist model");
 }
--- src/utilities.rs
+++ src/utilities.rs
@@ -1,7 +1,6 @@
 extern crate openblas_src;
 extern crate postgres;
 
-
 use std::fs::File;
 use std::io::Write;
 use std::ops::Div;
@@ -12,11 +11,11 @@ use plotters::prelude::*;
 use postgres::{Client, NoTls};
 use rand;
 use rand::Rng;
-use smartcore::linalg::BaseMatrix;
 use smartcore::linalg::naive::dense_matrix::DenseMatrix;
+use smartcore::linalg::BaseMatrix;
 use smartcore::math::num::RealNumber;
 // Model performance
-use smartcore::metrics::{mean_squared_error};
+use smartcore::metrics::mean_squared_error;
 use smartcore::model_selection::train_test_split;
 // Imports for KNN classifier
 use smartcore::neighbors::knn_classifier::KNNClassifier;
@@ -41,7 +40,8 @@ trait ToArray {
 
 impl ToArray for Track {
     fn to_array(&self) -> Array1<f64> {
-        let vector = Vec::from([self.danceability,
+        let vector = Vec::from([
+            self.danceability,
             self.energy,
             self.key.to_f64().unwrap(),
             self.loudness,
@@ -51,38 +51,10 @@ impl ToArray for Track {
             self.liveness,
             self.valence,
             self.tempo,
-            self.time_signature.to_f64().unwrap()
+            self.time_signature.to_f64().unwrap(),
         ]);
         return Array::from(arr1(&*vector));
     }
-}
-
-fn main() {
-    let mut dataset = get_tracks("albert").expect("can't get dataset from database.");
-    let y = fill_dataset_return_y(&mut dataset);
-    let records = Array::from_shape_vec(dataset.raw_dim(), dataset.iter().cloned().collect()).unwrap();
-    let x = DenseMatrix::from_array(
-        y.len(),
-        records.len_of(Axis(1)),
-        dataset.as_slice().unwrap(),
-    );
-    let (x_train, x_test, y_train, y_test) = train_test_split(&x, &y, 0.2, true);
-    let knn = KNNClassifier::fit(&x_train, &y_train, Default::default()).unwrap();
-
-    let predictions = knn.predict(&x_test).unwrap();
-    println!("MSE: {}", mean_squared_error(&y_test, &predictions));
-    {
-        let knn_bytes = bincode::serialize(&knn).expect("Can not serialize the model");
-        File::create("knn.model")
-            .and_then(|mut f| f.write_all(&knn_bytes))
-            .expect("Can not persist model");
-    }
-    scatterplot(
-        &x,
-        Some(&predictions.into_iter().map(|f| f as usize).collect()),
-        "test",
-    )
-        .unwrap();
 }
 
 pub fn get_dataset(table: &str) -> (Array2<f64>, Vec<f64>) {
@@ -95,8 +67,11 @@ pub fn get_dataset_kevin() -> (Array2<f64>, Vec<f64>) {
     let mut data_array = get_tracks("kevin").expect("can't get dataset for kevin");
     let mut targets: Vec<f64> = vec![0.0; data_array.nrows()];
     let mut i = 0;
-    for rowerik in get_tracks("erik").expect("can't get dataset for erik").rows() {
-        if (i < 54) {
+    for rowerik in get_tracks("erik")
+        .expect("can't get dataset for erik")
+        .rows()
+    {
+        if i < 54 {
             i += 1;
             data_array.push_row(rowerik).unwrap();
         }
@@ -105,7 +80,7 @@ pub fn get_dataset_kevin() -> (Array2<f64>, Vec<f64>) {
     return (data_array, targets);
 }
 
-pub fn fill_dataset_return_y(mut array: &mut Array2<f64>) -> Vec<f64> {
+pub fn fill_dataset_return_y(array: &mut Array2<f64>) -> Vec<f64> {
     let mut targets: Vec<f64> = vec![0.0; array.nrows()];
     for roweline in get_tracks("eline").expect("boom").rows() {
         array.push_row(roweline).unwrap();
@@ -136,8 +111,11 @@ pub fn fill_dataset_return_y(mut array: &mut Array2<f64>) -> Vec<f64> {
 }
 
 pub fn get_tracks(table: &str) -> Result<Array2<f64>, postgres::Error> {
-    let mut conn = Client::connect("postgresql://admin:fkdvaQmAK82a4cGfdZSQ8rH6cyE2gCac@localhost/rustspotify", NoTls)
-        .unwrap();
+    let mut conn = Client::connect(
+        "postgresql://admin:fkdvaQmAK82a4cGfdZSQ8rH6cyE2gCac@localhost/rustspotify",
+        NoTls,
+    )
+    .unwrap();
     let mut vec = Vec::new();
     let query = format!("SELECT danceability, energy, key, loudness, mode, speechiness, instrumentalness, liveness, valence, tempo, time_signature FROM {table}");
     let mut i = 0;
@@ -163,17 +141,17 @@ pub fn get_tracks(table: &str) -> Result<Array2<f64>, postgres::Error> {
     return Result::Ok(array);
 }
 
-fn to_array2<T: Copy>(source: &[Array1<T>]) -> Result<Array2<T>, impl std::error::Error> {
+pub fn to_array2<T: Copy>(source: &[Array1<T>]) -> Result<Array2<T>, impl std::error::Error> {
     let width = source.len();
     let flattened: Array1<T> = source.into_iter().flat_map(|row| row.to_vec()).collect();
     let height = flattened.len() / width;
     flattened.into_shape((width, height))
 }
 
-
 /// Draw a scatterplot of `data` with labels `labels`
 /// We use Plotters library to draw scatter plot.
 /// https://docs.rs/plotters/0.3.0/plotters/
+#[allow(dead_code)]
 pub fn scatterplot(
     data: &DenseMatrix<f64>,
     labels: Option<&Vec<usize>>,
@@ -196,10 +174,7 @@ pub fn scatterplot(
         .x_label_area_size(20)
         .y_label_area_size(20)
         .build_cartesian_2d(x_min..x_max, y_min..y_max)?;
-    scatter_ctx
-        .configure_mesh()
-        .disable_x_mesh()
-        .draw()?;
+    scatter_ctx.configure_mesh().disable_x_mesh().draw()?;
     match labels {
         Some(labels) => {
             scatter_ctx.draw_series(data_values.chunks(2).zip(labels.iter()).map(|(xy, &l)| {
@@ -219,38 +194,44 @@ pub fn scatterplot(
     Ok(())
 }
 
-pub fn scatterplot_vec(data: Vec<f64>,
-                       labels: &Vec<f64>,
-                       title: &str) {
-    let path = format!("{}.svg", title);
-    let root = SVGBackend::new(&path, (800, 600)).into_drawing_area();
+pub fn performance_graph(
+    path: &str,
+    caption: &str,
+    data: Vec<i32>,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let root = SVGBackend::new(path, (640, 480)).into_drawing_area();
 
-    let x_min = data.iter().fold(f64::INFINITY, |a, &b| a.min(b));
-    let x_max = data.iter().fold(f64::zero(), |a, &b| a.max(b));
-    let y_min = x_min;
-    let y_max = x_max + 1.0;
+    root.fill(&WHITE)?;
 
-    root.fill(&WHITE).unwrap();
-    let root = root.margin(15, 15, 15, 15);
+    let mut chart = ChartBuilder::on(&root)
+        .x_label_area_size(35)
+        .y_label_area_size(40)
+        .margin(5)
+        .caption(caption, ("sans-serif", 50.0))
+        .build_cartesian_2d(0..2, 0..100)?;
 
-    let mut scatter_ctx = ChartBuilder::on(&root)
-        .x_label_area_size(20)
-        .y_label_area_size(20)
-        .build_cartesian_2d(x_min..x_max, y_min..y_max).unwrap();
-    scatter_ctx
+    chart
         .configure_mesh()
         .disable_x_mesh()
-        .draw().unwrap();
-    let mut rng = rand::thread_rng();
-    scatter_ctx.draw_series(data.chunks(1).zip(labels.iter()).map(|(x, &l)| {
-        let jitter: f64 = rng.gen::<f64>().div(2.0);
-        EmptyElement::at((x[0] + jitter, l + jitter))
-            + Circle::new((0, 0), 3, ShapeStyle::from(&Palette99::pick(l as usize)).filled())
-            + Text::new(format!("{}", l), (6, 0), ("sans-serif", 15.0).into_font())
-    })).expect("can't draw for some reason");
+        .disable_y_mesh()
+        .bold_line_style(&WHITE.mix(0.3))
+        .y_desc("Count")
+        .x_desc("Bucket")
+        .axis_desc_style(("sans-serif", 15))
+        .draw()?;
+
+    chart.draw_series(
+        Histogram::vertical(&chart)
+            .style(BLUE.mix(0.7).filled())
+            .data(data.iter().enumerate().map(|(i, x)| (i as i32, *x))),
+    )?;
+
+    // To avoid the IO failure being ignored silently, we manually call the present function
+    root.present().expect("Unable to write result to file");
+    println!("Result has been saved to {}", path);
+
+    Ok(())
 }
-
-
 /// Get min value of `x` along axis `axis`
 pub fn min<T: RealNumber>(x: &DenseMatrix<T>, axis: usize) -> T {
     let n = x.shape().0;
@@ -267,78 +248,4 @@ pub fn max<T: RealNumber>(x: &DenseMatrix<T>, axis: usize) -> T {
         .iter()
         .max_by(|a, b| a.partial_cmp(b).unwrap())
         .unwrap()
-}
-
-/// Generates 2x2 mesh grid from `x`
-pub fn make_meshgrid(x: &DenseMatrix<f64>) -> DenseMatrix<f64> {
-    let n = x.shape().0;
-    let x_min = min(x, 0) - 1.0;
-    let x_max = max(x, 0) + 1.0;
-    let y_min = min(x, 1) - 1.0;
-    let y_max = max(x, 1) + 1.0;
-
-    let x_step = (x_max - x_min) / n as f64;
-    let x_axis: Vec<f64> = (0..n).map(|v| (v as f64 * x_step) + x_min).collect();
-    let y_step = (y_max - y_min) / n as f64;
-    let y_axis: Vec<f64> = (0..n).map(|v| (v as f64 * y_step) + y_min).collect();
-
-    let x_new: Vec<Vec<f64>> = x_axis
-        .clone()
-        .into_iter()
-        .flat_map(move |v1| y_axis.clone().into_iter().map(move |v2| vec![v1, v2]))
-        .collect();
-
-    DenseMatrix::from_2d_vec(&x_new)
-}
-
-/// Draw a mesh grid defined by `mesh` with a scatterplot of `data` on top
-/// We use Plotters library to draw scatter plot.
-/// https://docs.rs/plotters/0.3.0/plotters/
-pub fn scatterplot_with_mesh(
-    mesh: &DenseMatrix<f64>,
-    mesh_labels: &Vec<f64>,
-    data: &DenseMatrix<f64>,
-    labels: &Vec<f64>,
-    title: &str,
-) -> Result<(), Box<dyn std::error::Error>> {
-    let path = format!("{}.svg", title);
-    let root = SVGBackend::new(&path, (800, 600)).into_drawing_area();
-
-    root.fill(&WHITE)?;
-    let root = root.margin(15, 15, 15, 15);
-
-    let x_min = (min(mesh, 0) - 1.0) as f64;
-    let x_max = (max(mesh, 0) + 1.0) as f64;
-    let y_min = (min(mesh, 1) - 1.0) as f64;
-    let y_max = (max(mesh, 1) + 1.0) as f64;
-
-    let mesh_labels: Vec<usize> = mesh_labels.into_iter().map(|&v| v as usize).collect();
-    let mesh: Vec<f64> = mesh.iter().map(|v| v as f64).collect();
-
-    let labels: Vec<usize> = labels.into_iter().map(|&v| v as usize).collect();
-    let data: Vec<f64> = data.iter().map(|v| v as f64).collect();
-
-    let mut scatter_ctx = ChartBuilder::on(&root)
-        .x_label_area_size(20)
-        .y_label_area_size(20)
-        .build_cartesian_2d(x_min..x_max, y_min..y_max)?;
-    scatter_ctx
-        .configure_mesh()
-        .disable_x_mesh()
-        .disable_y_mesh()
-        .draw()?;
-    scatter_ctx.draw_series(mesh.chunks(2).zip(mesh_labels.iter()).map(|(xy, &l)| {
-        EmptyElement::at((xy[0], xy[1]))
-            + Circle::new((0, 0), 1, ShapeStyle::from(&Palette99::pick(l)).filled())
-    }))?;
-    scatter_ctx.draw_series(data.chunks(2).zip(labels.iter()).map(|(xy, &l)| {
-        EmptyElement::at((xy[0], xy[1]))
-            + Circle::new(
-            (0, 0),
-            3,
-            ShapeStyle::from(&Palette99::pick(l + 3)).filled(),
-        )
-    }))?;
-
-    Ok(())
 }
